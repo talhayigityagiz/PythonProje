3272950a01202a8d7e5169daedc93596c087a5d4
talhayigityagiz <talhayigityagiz@gmail.com>
2023-05-04 21:40:44 +0300

    Update filmcennetiHDizle.py

--- filmcennetiHDizle.py
+++ filmcennetiHDizle.py
@@ -1,1 +1,9 @@
 import csv
+
+class Database:
+    def __init__(self,nadideparcalar.):
+        self.nadideparcalar = nadideparcalar
+        
+    def bilgi_kaydet(self, bilgi):
+        with open(self.nadideparcalar, "a") as dosya:
+            dosya.write(bilgi + "\n")
