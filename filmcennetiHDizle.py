97636d5d454cac13c88c7bbd91bde649a61fc6b7
talhayigityagiz <talhayigityagiz@gmail.com>
2023-05-06 13:14:04 +0300

    güncelleme

--- filmcennetiHDizle.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-class Database:
-    def __init__(self, filmİsmi, imdbPuani, filmYil, filmTür, filmSüre, filmFragman):
-        self.nadideparcalar = nadideparcalar
-        
-    def bilgi_kaydet(self, bilgi):
-        with open(self.nadideparcalar, "a") as dosya:
-            dosya.write(bilgi + "\n")
